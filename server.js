f14663280c47d21f8bd69fc886139965e1f2376a
Ayesha Jownally <sj.ayesha@gmail.com>
2017-02-28 21:55:59 +0400

    session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let app = express()//charger expressjs
 let bodyParser = require('body-parser')
+let session = require('express-session')
 
 //moteur de template
 app.set('view engine', 'pug')
@@ -9,18 +10,29 @@ app.set('view engine', 'pug')
 app.use('/assets', express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(session({
+  secret: 'ayesha',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
 //gerer mon util
 //a la page racine lance une fonction
 //ROUTE
 app.get('/', (request, response) => {
-    response.render('pages/index', {test: 'Salut'})
+    if (request.session.error) {
+        response.locals.error = request.session.error
+        request.session.error = undefined
+    }
+    response.render('pages/index')
 })
 
 //route
 app.post('/', (request, response) => {
     if (request.body.message === undefined || request.body.message === '') {
-        response.render('pages/index', {error: "Vous n'avez pas rentre de message :("})
+        request.session.error = "Il y a une erreur!"
+        response.redirect('/')
     }
 })
 
